0874c3fdb2eea88940f3a1fb6b5826f8e03abb70
DuncanRL <59705125+DuncanRuns@users.noreply.github.com>
2022-03-01 20:04:17 -0400

    VERSION

--- src/main/java/me/duncanruns/fsg/FSG115.java
+++ src/main/java/me/duncanruns/fsg/FSG115.java
@@ -7,8 +7,10 @@ import java.util.Random;
 
 public class FSG115 {
 
+    public static final String VERSION = "1.1.0";
+
     public static void main(String[] args) {
-        System.out.println("--------------------\nMinecraft 1.15.2 FSG by DuncanRuns\nRunning on Java " + System.getProperty("java.version") + "\n--------------------");
+        System.out.println("--------------------\nMC1.15 FSG v" + VERSION + " by DuncanRuns\nRunning on Java " + System.getProperty("java.version") + "\n--------------------");
         System.out.println(findSeed(true));
     }
 
