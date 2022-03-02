ecae700389be676634412f424ab61164d4024f3a
DuncanRL <59705125+DuncanRuns@users.noreply.github.com>
2022-03-01 20:05:49 -0400

    space bar lmao frick

--- src/main/java/me/duncanruns/fsg/FSG115.java
+++ src/main/java/me/duncanruns/fsg/FSG115.java
@@ -10,7 +10,7 @@ public class FSG115 {
     public static final String VERSION = "1.1.0";
 
     public static void main(String[] args) {
-        System.out.println("--------------------\nMC1.15 FSG v" + VERSION + " by DuncanRuns\nRunning on Java " + System.getProperty("java.version") + "\n--------------------");
+        System.out.println("--------------------\nMC 1.15 FSG v" + VERSION + " by DuncanRuns\nRunning on Java " + System.getProperty("java.version") + "\n--------------------");
         System.out.println(findSeed(true));
     }
 
